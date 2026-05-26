3a31ffd3a9a1928f78ad8d19cdfb326a3a2c0a25
Tim <tim@local.ubt>
2026-05-26 16:38:49 +0200

    Add tasks to sidebar
    - Make sidebar retrieve and display tasks

--- service/TaskManager.Api/Program.cs
+++ service/TaskManager.Api/Program.cs
@@ -2,7 +2,20 @@ using System.Text.Json.Serialization;
 using TaskManager.Api.Repositories;
 using TaskManager.Api.Services;
 
+const string CorsPolicy = "AllowVueClient";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(CorsPolicy, policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     {
@@ -25,6 +38,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(CorsPolicy);
 
 app.MapControllers();
 
